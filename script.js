b4c9be03017ac0d4bd4c686de46aa27fbe8ff347
sharmaayush89050-beep <sharmaayush89050@gmail.com>
2025-11-05 12:20:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,23 +36,4 @@ function performSearch() {
     }
 }
 
-
-// Slideshow Code 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
+// Slideshow ka code yahaan se hata diya gaya hai.
